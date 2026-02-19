d7d2b92defdf47786448d5ac50642717e540e826
sandeshkhadka <pqrsandesh@gmail.com>
2026-02-19 23:08:18 +0545

    implement mpris client

---
+++ src/mpris.cpp
@@ -0,0 +1,203 @@
+#include "mpris.hpp"
+#include <cstdint>
+#include <iostream>
+#include <sdbus-c++/Error.h>
+#include <sdbus-c++/IConnection.h>
+#include <sdbus-c++/IProxy.h>
+#include <sdbus-c++/Types.h>
+#include <string>
+#include <utility>
+#include <vector>
+
+std::string MprisClient::DBUS_SERVICE_NAME = "org.freedesktop.DBus";
+std::string MprisClient::DBUS_OBJECT_PATH = "/org/freedesktop/DBus";
+std::string MprisClient::DBUS_PROPS_IFACE = "org.freedesktop.DBus.Properties";
+std::string MprisClient::MPRIS_SERVICE_PREFIX = "org.mpris.MediaPlayer2";
+std::string MprisClient::MPRIS_OBJECT_PATH = "/org/mpris/MediaPlayer2";
+std::string MprisClient::MPRIS_PLAYER_IFACE = "org.mpris.MediaPlayer2.Player";
+
+void MprisClient::PrintCurrentTrackInfo() {
+  std::string message;
+  if (m_trackInfo.title.size() <= 0)
+    return;
+  message += m_trackInfo.title;
+  if (m_trackInfo.artist.size() > 0) {
+    message += " by '" + m_trackInfo.artist + "'";
+  }
+  if (m_trackInfo.artist.size() > 0) {
+    message += " from Album: " + m_trackInfo.album;
+  }
+  std::cout << message << std::endl;
+}
+
+TrackInfo MprisClient::ParseMetadata(
+  const std::map<std::string, sdbus::Variant>& metadata) {
+  TrackInfo info;
+  auto getStr = [&](const char* key) -> std::string {
+    auto it = metadata.find(key);
+    if (it != metadata.end()) {
+      try {
+        return it->second.get<std::string>();
+      } catch (...) {}
+    }
+    return "";
+  };
+
+  auto getStrArray = [&](const char* key) -> std::string {
+    auto it = metadata.find(key);
+    if (it != metadata.end()) {
+      try {
+        auto arr = it->second.get<std::vector<std::string>>();
+        if (!arr.empty())
+          return arr[0];
+      } catch (...) {}
+    }
+    return "";
+  };
+
+  auto getInt = [&](const char* key) -> int64_t {
+    auto it = metadata.find(key);
+    if (it != metadata.end()) {
+      try {
+        return it->second.get<int64_t>();
+      } catch (...) {}
+    }
+    return 0;
+  };
+
+  info.title = getStr("xesam:title");
+  info.artist = getStrArray("xesam:artist");
+  info.album = getStr("xesam:album");
+  info.duration_us = getInt("mpris:length");
+
+  return info;
+}
+
+MprisClient::MprisClient() {
+  try {
+    m_session = sdbus::createSessionBusConnection();
+    m_dbus_proxy =
+      sdbus::createProxy(*m_session,
+                         sdbus::ServiceName{MprisClient::DBUS_SERVICE_NAME},
+                         sdbus::ObjectPath{MprisClient::DBUS_OBJECT_PATH});
+
+    std::cout << "Connected to session bus.\n";
+    m_dbus_proxy->uponSignal("NameOwnerChanged")
+      .onInterface(MprisClient::DBUS_SERVICE_NAME)
+      .call([this](const std::string& name,
+                   const std::string& oldOwner,
+                   const std::string& newOwner) {
+        OnNameOwnerChange(name, oldOwner, newOwner);
+      });
+  } catch (sdbus::Error e) {
+    std::cerr << "Failed to create a session bus: " << e.getMessage()
+              << std::endl;
+    exit(1);
+  }
+  MprisClient::FindAndWatchPlayer();
+}
+
+void MprisClient::InvalidateCurrentPlayer() {
+  m_currentPlayerName.clear();
+  m_trackInfo.Clear();
+  m_currentPlayerProxy = nullptr;
+}
+
+void MprisClient::OnNameOwnerChange(const std::string& name,
+                                    const std::string& oldOwner,
+                                    const std::string& newOwner) {
+  if (name.rfind(MprisClient::MPRIS_SERVICE_PREFIX) != std::string::npos) {
+    if (!newOwner.empty() && m_currentPlayerName.empty()) {
+      WatchPlayer(name);
+    } else if (!oldOwner.empty() && name == m_currentPlayerName) {
+      InvalidateCurrentPlayer();
+      MprisClient::FindAndWatchPlayer();
+    }
+  }
+}
+
+std::vector<std::string> MprisClient::SearchAvailablePlayers() {
+  std::vector<std::string> all;
+  std::vector<std::string> players;
+  m_dbus_proxy->callMethod("ListNames")
+    .onInterface(MprisClient::DBUS_SERVICE_NAME)
+    .storeResultsTo(all);
+
+  for (const auto& c : all) {
+    if (c.find(MprisClient::MPRIS_SERVICE_PREFIX) != std::string::npos) {
+      players.push_back(c);
+    }
+  }
+  return players;
+}
+
+void MprisClient::WatchPlayer(const std::string& name) {
+  if (name == m_currentPlayerName) {
+    return;
+  }
+  auto proxy =
+    sdbus::createProxy(*m_session,
+                       sdbus::ServiceName{name},
+                       sdbus::ObjectPath{MprisClient::MPRIS_OBJECT_PATH});
+
+  proxy->uponSignal("PropertiesChanged")
+    .onInterface(MprisClient::DBUS_PROPS_IFACE)
+    .call([this](const std::string& iface,
+                 const std::map<std::string, sdbus::Variant>& changed,
+                 const std::vector<std::string>& invalidated) {
+      OnPropertiesChanged(iface, changed, invalidated);
+    });
+
+  proxy->uponSignal("Seeked")
+    .onInterface("org.mpris.MediaPlayer2.Player")
+    .call([this](int64_t newPosition) {
+      this->m_trackInfo.m_seeked_pos = newPosition;
+    });
+  try {
+    sdbus::Variant metadataVar;
+    proxy->callMethod("Get")
+      .onInterface(DBUS_PROPS_IFACE)
+      .withArguments(MPRIS_PLAYER_IFACE, "Metadata")
+      .storeResultsTo(metadataVar);
+
+    auto metadata = metadataVar.get<std::map<std::string, sdbus::Variant>>();
+    m_trackInfo = ParseMetadata(metadata);
+  } catch (sdbus::Error& e) {
+    std::cerr << "Falied to set intial trackinfo: " << e.getMessage() << "\n";
+  }
+
+  std::cout << "Watching Player: " << name << std::endl;
+  if (m_trackInfo.IsValid()) {
+    std::cout << "Playing Right Now: ";
+    PrintCurrentTrackInfo();
+  }
+
+  m_currentPlayerProxy = std::move(proxy);
+  m_currentPlayerName = name;
+}
+
+void MprisClient::FindAndWatchPlayer() {
+  std::vector<std::string> playerNames = MprisClient::SearchAvailablePlayers();
+  for (const auto& p : playerNames) {
+    WatchPlayer(p);
+    break;
+  }
+}
+
+void MprisClient::OnPropertiesChanged(
+  const std::string& iface,
+  const std::map<std::string, sdbus::Variant>& changed,
+  const std::vector<std::string>& invalidated) {
+  for (auto& [k, v] : changed) {
+    if (k == "Metadata") {
+      m_trackInfo =
+        ParseMetadata(v.get<std::map<std::string, sdbus::Variant>>());
+      std::cout << "Changed Track. \n Now Playing: ";
+      PrintCurrentTrackInfo();
+    }
+  }
+}
+
+void MprisClient::EnterMainLoop() {
+  m_session->enterEventLoop();
+}
---
+++ src/mpris.hpp
@@ -0,0 +1,78 @@
+#include <cstdint>
+#include <memory>
+#include <sdbus-c++/Error.h>
+#include <sdbus-c++/IConnection.h>
+#include <sdbus-c++/IProxy.h>
+#include <sdbus-c++/Types.h>
+#include <string>
+#include <vector>
+
+struct TrackInfo {
+  std::string title;
+  std::string artist;
+  std::string album;
+  int64_t duration_us = 0; // Microseconds
+  int64_t m_seeked_pos;
+
+  bool IsValid() const {
+    return !title.empty();
+  }
+
+  void Clear() {
+    title.clear();
+    artist.clear();
+    album.clear();
+    duration_us = 0;
+    m_seeked_pos = 0;
+  }
+
+  bool operator==(const TrackInfo& other) const {
+    return title == other.title && artist == other.artist &&
+           album == other.album;
+  }
+
+  bool operator!=(const TrackInfo& other) const {
+    return !(*this == other);
+  }
+};
+
+class MprisClient {
+private:
+  static std::string DBUS_SERVICE_NAME;
+  static std::string DBUS_OBJECT_PATH;
+  static std::string DBUS_PROPS_IFACE;
+  static std::string MPRIS_SERVICE_PREFIX;
+  static std::string MPRIS_OBJECT_PATH;
+  static std::string MPRIS_PLAYER_IFACE;
+
+  std::unique_ptr<sdbus::IProxy> m_currentPlayerProxy;
+  std::string m_currentPlayerName;
+  std::unique_ptr<sdbus::IConnection> m_session;
+  std::unique_ptr<sdbus::IProxy> m_dbus_proxy;
+
+  TrackInfo m_trackInfo;
+
+  std::vector<std::string> SearchAvailablePlayers();
+  void FindAndWatchPlayer();
+  void OnPropertiesChanged(const std::string& iface,
+                           const std::map<std::string, sdbus::Variant>& changed,
+                           const std::vector<std::string>& invalidated);
+
+  static void PrintMetadata(const sdbus::Variant&);
+
+  void WatchPlayer(const std::string& name);
+  void InvalidateCurrentPlayer();
+
+  TrackInfo
+  ParseMetadata(const std::map<std::string, sdbus::Variant>& metadata);
+
+  void OnNameOwnerChange(const std::string& name,
+                         const std::string& oldOwner,
+                         const std::string& newOwner);
+
+  void PrintCurrentTrackInfo();
+
+public:
+  MprisClient();
+  void EnterMainLoop();
+};
